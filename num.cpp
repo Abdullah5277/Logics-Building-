cf726974a7b7efa3b6c06d2bdb0d1d1ec5fce70d
Abdullah <abdullahramzaan02@gmail.com>
2026-09-02 11:02:32 +0500

    Add files via upload

---
+++ num.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num =1;
+    for(int i=1;i<6;i++){
+       
+          cout<<"";
+      
+        for(int j=0;j<i;j++){
+
+            cout<<num;
+            num++;
+        }
+       
+        cout<<"\n";
+
+    }
+    return 0;
+}
