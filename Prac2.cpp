cacb4bb5b4938cad6e7760d5df3362efd13506a0
Abdullah <abdullahramzaan02@gmail.com>
2026-09-09 11:41:58 +0500

    Add files via upload

---
+++ Prac2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    for (int i=0;i<5;i++){
+        for(int k=0;k<5;k++){
+            cout<<" ";
+            for(int j=i;j<5;j++){
+            cout<<j;
+        }
+        }
+        
+        cout<<"\n";
+    }
+    for (int i=0;i<=5;i++){
+        for(int k=0;k<5;k++){
+            cout<<"";
+        }
+        for(int j=0;j<i;j++){
+            cout<<j;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
