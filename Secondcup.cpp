958fde11cac9a626d77f63c5dfa92cc87829a45d
Abdullah <abdullahramzaan02@gmail.com>
2026-09-14 21:20:19 +0500

    Add files via upload

---
+++ Secondcup.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    for(int i=0;i<5;i++){
+        for(int k=0;k<i;k++){
+            cout<<" *";
+        }
+        for(int j=5;j>i;j--){
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+
+    for(int i=2;i<=5;i++){
+        for(int k=5;k>i;k--){
+            cout<<" *";
+        }
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
